39454d2ef2819ca2989e5d7534231ee233a27820
JRisJunior <jeffmrubin@gmail.com>
2022-12-27 13:16:56 -0600

    add sessions controller + add sessions routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,8 @@ Rails.application.routes.draw do
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
+
+  get "/login" => "sessions#new"
+  post "/sessions" => "sessions#create"
+  get "/logout" => "sessions#destroy"
 end
