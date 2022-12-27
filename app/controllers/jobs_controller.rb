fb9be86f75a0ee66db4a5a71867dcd6fe030f466
JRisJunior <jeffmrubin@gmail.com>
2022-12-27 14:28:41 -0600

    add create action and route for jobs, still needs work

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -3,4 +3,21 @@ class JobsController < ApplicationController
     @jobs = Job.all
     render "jobs/index"
   end
+
+  def create
+    if current_user && current_user.admin_level == true
+      @job = Job.new(
+        title: params[:job][:name],
+        description: params[:job][:description],
+        url: params[:job][:url],
+        location: params[:job][:location],
+        active: params[:job][:active],
+        salary_range: params[:job][:salary_range]
+      )
+      if @job.save
+        render "jobs/index"
+      else
+        render html: "Your job did not save correctly. Please try again."
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/jobs" => "jobs#index"
+  post "/jobs" => "jobs#create"
 
   get "/signup" => "users#new"
   post "/users" => "users#create"
