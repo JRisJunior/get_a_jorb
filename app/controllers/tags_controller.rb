657a0b4295e8b9f66e935e9d64c25df02704f704
Juwon Do <nobeldr13@gmail.com>
2022-12-28 15:13:33 -0600

    create jobposttag in job create action

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -15,11 +15,15 @@ class TagsController < ApplicationController
   end
 
   def create
-    @tag = Tag.new(
-      name: params[:tag][:name]
-    )
-    @tag.save
-    redirect_to "/tags"
+    if current_user && current_user.admin_level == true
+      @tag = Tag.new(
+        name: params[:tag][:name]
+      )
+      @tag.save
+      redirect_to "/tags"
+    else
+      render html: "WHAT ARE YOU DOING HERE"
+    end
   end
-  
+
 end
